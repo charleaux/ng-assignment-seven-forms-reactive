5822d5241678cc87e379b1f9163cf101f4f1ad40
miamidade\e314637 <charles.torry@miamidade.gov>
2019-03-12 16:56:59 -0400

    cleanups for not needed imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
